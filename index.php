991c16268ea232f4740f33fa8148e832dcc89e95
Danilo Santos <danilo.carreiro@etec.sp.gov.br>
2018-08-29 20:35:25 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+
+});
+
+$app->get('/home',function(){
+
+});
+
+$app->run();
+
+ ?>
