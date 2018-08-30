dbccc82ecee5db361689e9c504cd0893c9a2db85
Danilo Santos <danilo.carreiro@etec.sp.gov.br>
2018-08-30 00:58:23 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php 
+session_name('lojavirtual');
+session_start();
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -10,17 +13,43 @@ $app->config('debug', true);
 
 $app->get('/', function() {
     
-	$page=new Page(['data'=>['titulo'=>'paginas Inicial']]);
+	$page=new Page();
 	$page->setTpl('index');
     
 });
 
 $app->get('/admin', function() {
+    User::verifyLogin();
+	$page=new PageAdmin();
+	$page->setTpl('index'); 
     
-	$page=new PageAdmin(['data'=>['titulo'=>'paginas Inicial']]);
-	$page->setTpl('index');
+}); 
+
+$app->get('/admin/login', function() {
+    
+	$page=new PageAdmin([
+		'header'=>false,
+		'footer'=>false
+
+	]);
+	$page->setTpl('login'); 
     
 });
+
+$app->post('/admin/login', function() {
+	User::login($_POST['login'],$_POST['password']);
+	header("location:/admin");
+	exit;
+    
+});
+
+$app->get('/admin/logout', function() {
+	User::logout();
+	header("location:/admin/login");
+	exit;  
+});
+
+
 
 
 $app->run();
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -7,15 +7,17 @@ class Page {
 	private $tpl;
 	private $options=[];
 	private $defaults=[
+	   "header"=>true,
+	   'footer'=>true,
        "data"=>[]
 	];
 
-	public function __construct($opts=array(),$tpl_dir="/views/"){
+	public function __construct($opts=array(),$tpl_dir="views/"){
 		    $this->options=array_merge($this->defaults,$opts);
 		
 			$config = array(
                             "base_url"	    => "http://www.hcodecommerce.com.br/", 
-							"tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].$tpl_dir,
+							"tpl_dir"       => $_SERVER['DOCUMENT_ROOT'].'/'.$tpl_dir,
 							"cache_dir"     => $_SERVER['DOCUMENT_ROOT']."/views-cache/",
 
 						   );
@@ -26,7 +28,7 @@ class Page {
 
 	        $this->setData($this->options['data']);
 
-	        $this->tpl->draw("header");
+	       if($this->options['header']===true) $this->tpl->draw("header");
 
 	}
 
@@ -46,8 +48,7 @@ class Page {
 	}
 
 	public function __destruct(){
-          
-          $this->tpl->draw('footer');
+        if($this->options['footer']===true)  $this->tpl->draw('footer');
 	}
 }
 
